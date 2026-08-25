d650410101c2febbbd7661301d2937811f133212
JERONIMORAMIREZRESTREPO <jeronimoramirez1159288@correo.itm.edu.co>
2026-08-24 19:44:15 -0500

    Add domain entities, AppDbContext and initial migration (Module 2)

---
+++ GolBet.Entities/Enums/BetPick.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// GolBet.Entities/Enums/BetPick.cs 
+namespace GolBet.Entities.Enums;
+public enum BetPick
+{
+    Home = 0,
+    Draw = 1,
+    Away = 2
+}
